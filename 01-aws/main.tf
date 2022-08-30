f15f625dbdef3eb434715ac498b01fcbf6a258d3
Faith Odonghanro <toriboi.fo@gmail.com>
2022-08-30 12:03:02 +0100

    Lesson 1

---
+++ 01-aws/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "hive_cli"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
